3e0964579e075b4b3b948245dd565a319c6c263c
ouo_yoonk <joyk9251@gmail.com>
2021-11-04 22:52:04 +0900

    당근 화면에 뿌리기
    - 생성자 함수로 당근 생성
    - 인스턴스 생성하는 함수 생성 및 당근 10개 만들기
    - 마우스오버 효과 적용

--- carrot/carrot.js
+++ carrot/carrot.js
@@ -1,66 +1,35 @@
-// 재생버튼
+// 생성자 함수 사용
+function Carrot(x, y) {
+  // TODO : this가 가리키는 것은?
 
-const playButton = document.querySelector('.play-button');
-const stopIcon = document.querySelector('.fa-stop');
-const startIcon = document.querySelector('.fa-play');
+  const carrot = document.createElement('img');
+  const ground = document.querySelector('.ground');
+  carrot.src = './img/carrot.png';
+  carrot.classList.add('carrot');
+  ground.appendChild(carrot);
 
-let isPlaying = false;
+  console.log(x, y);
+}
 
-const startGame = () => {
-  isPlaying = true;
-  startTimer();
-  changeToStopButton();
-};
-
-const stopGame = () => {
-  isPlaying = false;
-  stopTimer();
-  changeToPlayButton();
-};
-
-const endGame = () => {
-  isStart = false;
-  stopTimer();
-  changeToPlayButton();
-};
-
-const changeToPlayButton = () => {
-  stopIcon.style.display = 'none';
-  startIcon.style.display = 'inline';
-};
-
-const changeToStopButton = () => {
-  stopIcon.style.display = 'inline';
-  startIcon.style.display = 'none';
-};
-
-playButton.addEventListener('click', () => {
-  if (isPlaying) {
-    console.log('멈춰');
-    stopGame();
-  } else {
-    console.log('다시시작');
-    startGame();
+// TODO : 프로토타입으로 구현하려면..?
+Carrot.prototype = {
+  construct: Carrot,
+  init: function () {
+    const self = this;
+    console.log(self.carrot);
+    self.carrot.addEventListener(
+      ('click',
+      () => {
+        console.log('ㅎㅎ');
+      })
+    );
   }
-});
-
-// 타이머
-const timer = document.querySelector('.timer');
-let timeCount = 10;
-let interval;
-
-const startTimer = () => {
-  interval = setInterval(() => {
-    if (timeCount <= 0) {
-      endGame();
-    } else {
-      timeCount--;
-    }
-    timer.innerHTML = timeCount;
-  }, 1000);
 };
 
-const stopTimer = () => {
-  clearInterval(interval);
-  console.log('clear');
+const paintCarrot = () => {
+  for (let i = 1; i <= 10; i++) {
+    const x = '101px';
+    const y = '11px';
+    new Carrot(x, y);
+  }
 };
---
+++ carrot/game.js
@@ -0,0 +1,64 @@
+// 재생버튼
+
+const playButton = document.querySelector('.play-button');
+const stopIcon = document.querySelector('.fa-stop');
+const startIcon = document.querySelector('.fa-play');
+
+let isPlaying = false;
+
+const startGame = () => {
+  isPlaying = true;
+  startTimer();
+  changeToStopButton();
+  paintCarrot();
+};
+
+const stopGame = () => {
+  isPlaying = false;
+  stopTimer();
+  changeToPlayButton();
+};
+
+const endGame = () => {
+  changeToPlayButton();
+  isStart = false;
+  stopTimer();
+};
+
+const changeToPlayButton = () => {
+  stopIcon.style.display = 'none';
+  startIcon.style.display = 'inline';
+};
+
+const changeToStopButton = () => {
+  stopIcon.style.display = 'inline';
+  startIcon.style.display = 'none';
+};
+
+playButton.addEventListener('click', () => {
+  if (isPlaying) {
+    stopGame();
+  } else {
+    startGame();
+  }
+});
+
+// 타이머
+const timer = document.querySelector('.timer');
+let timeCount = 10;
+let interval;
+
+const startTimer = () => {
+  interval = setInterval(() => {
+    if (timeCount <= 0) {
+      endGame();
+    } else {
+      --timeCount;
+    }
+    timer.innerHTML = timeCount;
+  }, 1000);
+};
+
+const stopTimer = () => {
+  clearInterval(interval);
+};
